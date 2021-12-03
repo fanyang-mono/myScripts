2eaf5a8fa7f7915e8a15d1f6d03b7f68ac9ae8bb
Fan Yang <yangfan@microsoft.com>
2021-12-03 07:25:29 -0800

    Add script to build and run microbenchmarks

--- buildMicrobenchmarks.sh
+++ buildMicrobenchmarks.sh
@@ -6,12 +6,16 @@
 
 export RuntimeRepoRootDir=/home/yangfan/work/runtime
 export MicrobenchmarksRepoRootDir=/home/yangfan/work/performance
+export DotnetSdkInstallationScriptDir=/home/yangfan/work
 
 export RELEASE_NUM=7
 export CONFIG=Release
 export ARCH=arm64
 export OS=Linux
 export MYDOTNET=$RuntimeRepoRootDir/.dotnet-mono/dotnet
+export RELEASE_SDK=6
+export SdkVerNum=7.0.100-alpha.1.21558.2
+export RuntimeVerNum=6.0.0-rc.2.21452.2
 
 export OriginDir=$PWD
 
@@ -19,7 +23,7 @@ build_repo()
 {
     cd $RuntimeRepoRootDir
     ./build.sh mono+libs+clr -c $CONFIG
-    src/test/build.sh generatelayoutonly $CONFIG
+    src/tests/build.sh generatelayoutonly $CONFIG
     cd $OriginDir
 }
 
@@ -31,6 +35,12 @@ patch_mono()
         rm -rf .dotnet-mono
     fi
     mkdir $RuntimeRepoRootDir/.dotnet-mono
+
+    # install dotnet sdk
+    $DotnetSdkInstallationScriptDir/dotnet-install.sh -Architecture arm64 -InstallDir $RuntimeRepoRootDir/.dotnet-mono -NoPath -Version $SdkVerNum
+    # $DotnetSdkInstallationScriptDir/dotnet-install.sh -Architecture arm64 -InstallDir $RuntimeRepoRootDir/.dotnet-mono -NoPath -Version $RuntimeVerNum -Runtime dotnet
+    # cp -r $RuntimeRepoRootDir/.dotnet/* $RuntimeRepoRootDir/.dotnet-mono
+
     ./build.sh -subset libs.pretest -configuration $CONFIG -ci -arch $ARCH -testscope innerloop /p:RuntimeArtifactsPath=$RuntimeRepoRootDir/artifacts/bin/mono/$OS.$ARCH.$CONFIG /p:RuntimeFlavor=mono
     cp -rf $RuntimeRepoRootDir/artifacts/bin/runtime/net$RELEASE_NUM.0-$OS-$CONFIG-$ARCH/* $RuntimeRepoRootDir/artifacts/bin/testhost/net$RELEASE_NUM.0-$OS-$CONFIG-$ARCH/shared/Microsoft.NETCore.App/$RELEASE_NUM.0.0
     cp -r $RuntimeRepoRootDir/artifacts/bin/testhost/net$RELEASE_NUM.0-$OS-$CONFIG-$ARCH/* $RuntimeRepoRootDir/.dotnet-mono
@@ -40,10 +50,16 @@ patch_mono()
 
 build_microbenchmarks()
 {
-    echo "Warning: In order to build microbenchmarks successfully, you need to add \";netcoreapp$RELEASE_NUM.0\" to TargetFrameworks in file src/harness/BenchmarkDotNet.Extensions/BenchmarkDotNet.Extensions.csproj"
+    echo "Warning: In order to build microbenchmarks successfully, you need to add \";netcoreapp$RELEASE_SDK.0\" to TargetFramework and change TargetFramework to TargetFrameworks in file src/harness/BenchmarkDotNet.Extensions/BenchmarkDotNet.Extensions.csproj"
     cd $MicrobenchmarksRepoRootDir/src/harness/BenchmarkDotNet.Extensions
     $MYDOTNET build -c Release
     cd $OriginDir
+}
+
+run_microbenchmarks()
+{
+    cd $MicrobenchmarksRepoRootDir/src/benchmarks/micro
+    $MYDOTNET run -c Release -f net$RELEASE_SDK.0  MicroBenchmarks.csproj --filter System.Tests.Perf_Guid.EqualsSame --corerun $RuntimeRepoRootDir/.dotnet-mono/shared/Microsoft.NETCore.App/$RELEASE_NUM.0.0/corerun
 }
 
 main_fcn()
@@ -61,10 +77,26 @@ main_fcn()
             build_microbenchmarks
             ;;
 
+        run_microbenchmarks)
+            run_microbenchmarks
+            ;;
+        
+        build_and_run_microbenchmarks)
+            build_microbenchmarks
+            run_microbenchmarks
+            ;;
+
+        build_all)
+            build_repo
+            patch_mono
+            build_microbenchmarks
+            ;;
+
         all)
             build_repo
-            path_mono
+            patch_mono
             build_microbenchmarks
+            run_microbenchmarks
             ;;
     esac
 }
@@ -75,6 +107,8 @@ if [ -z "$1" ]; then
             * build_repo
             * patch_mono
             * build_microbenchmarks
+            * run_microbenchmarks
+            * build_all
             * all"
 else
     main_fcn $1
